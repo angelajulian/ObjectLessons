a0f2235aecc3de162f90c119f7113996c212753b
Angela M Julian <angela.julian@gmail.com>
2022-09-08 18:42:56 -0700

    completed 01-keys-in-objects added gitignore installed packages

--- problems/01-keys-in-object.js
+++ problems/01-keys-in-object.js
@@ -1,20 +1,32 @@
 /***********************************************************************
-Write a function called `keysInObject(obj)` that takes in an object and returns 
+Write a function called `keysInObject(obj)` that takes in an object and returns
 an array of all the keys within that Object.
 
 Do this once using using a `for...in` loop and once using `Object.keys`.
 
 Examples:
 
-let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
-let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
+
 keysInObject(animals); // => ["dog", "cat", "bison"]
 keysInObject(foods); // => ["apple", "lemon", "mango"]
 ***********************************************************************/
+let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
+let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
 
 function keysInObject(obj) {
-  // Your code here
+  return Object.keys(obj)
 }
 
+function kIo(obj) {
+  let arr = []
+  for (item in obj) {
+    arr.push(item)
+  }
+  return arr;
+}
+
+
+console.log(keysInObject(animals))
+console.log(kIo(foods))
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keysInObject;
